d184db96cfdde15213356d0033787f1e96e9acc0
ages3408 <ages3408@gmail.com>
2021-12-10 16:31:50 +0900

    1.1

---
+++ src/routes/Auth.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+//자동으로 import 되는 코드
+const Auth = () => <span>Auth</span>;
+export default Auth;
+//export default () => <span>Auth</span>
---
+++ src/routes/EditProfile.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const EditProfile = () => <span>EditProfile</span>;
+export default EditProfile;
+
+// export default () => <span>EditProfile</span>
