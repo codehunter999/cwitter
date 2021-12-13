72d777098850e45f24f0ce7c3fdfbf2ea002f1cb
ages3408 <ages3408@gmail.com>
2021-12-13 11:09:11 +0900

    1.2 repair

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,23 +3,25 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-export default() => {
+const AppRouter = () => {
     const [isLoggedIn, setIsLiggedIn] = useState(false);
     return(
         <Router>
             <Switch>
                 {isLoggedIn ? (
                 <>
-                <Route exact path="/">
-                    <Home />
-                </Route>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
                 </> 
                 ) : (
-                <Route exact path="/">
-                    <Auth />
-                </Route>)}
+                    <Route exact path="/">
+                        <Auth />
+                    </Route>
+                )
+                }
             </Switch>
         </Router>
     )
-    
-}
+};
+export default AppRouter;
